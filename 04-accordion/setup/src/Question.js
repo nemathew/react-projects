20c10ef371eced7e8f43f9fe725b42980dca0240
nicmathews <nicmathews@paypal.com>
2021-06-02 22:00:11 -0700

    Accordion Project complete

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = ({ title, info }) => {
+  const [isShown, setIsShown] = useState(false)
 
-export default Question;
+  const changeIsShown = () => {
+    setIsShown(!isShown)
+  }
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setIsShown(!isShown)}>
+          {isShown ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isShown && <p>{info}</p>}
+    </article>
+  )
+}
+
+export default Question
